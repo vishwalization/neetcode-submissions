9efc9dbad48cc0edc3b7a0d6c1314797ee9a2c98
vishwalization <182189991+vishwalization@users.noreply.github.com>
2026-07-26 13:16:31 +0530

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.py
@@ -0,0 +1,27 @@
+class MinStack:
+
+    def __init__(self):
+        self.st = []
+        self.mins = []
+        
+
+    def push(self, val: int) -> None:
+        if not self.mins or self.mins[-1] >= val:
+            self.mins.append(val)
+        self.st.append(val)
+        
+
+    def pop(self) -> None:
+        if self.mins and self.mins[-1] == self.top():
+            self.mins.pop()
+        self.st.pop()
+        
+
+    def top(self) -> int:
+        return self.st[-1]
+        
+
+    def getMin(self) -> int:
+        return self.mins[-1]
+
+        
